af50b04369752be241b3796efc104127611d9bd6
released <released@users.noreply.github.com>
2020-09-24 10:51:17 +0800

    Initial commit

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,398 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V1.00
+ * @brief    MP3 player sample plays MP3 files stored on SD memory card
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+#include "config.h"
+#include "diskio.h"
+#include "ff.h"
+
+#include "i2c_master.h"
+
+#define USE_M487_EVM_FREE_I2S
+#define USE_M487_EVM_FREE_I2C
+
+//#define USE_CUSTOMIZE_BOARD
+
+#define USE_NAU88C10_12M		(12000000ul)
+#define USE_NAU88C10_12_288M	(12288000ul)
+//#define USE_MCLK_12M			/*put in KEIL compiler*/
+//#define USE_MCLK_12_288M		/*put in KEIL compiler*/
+
+//uint32_t volatile u32BuffPos = 0;
+FATFS FatFs[FF_VOLUMES];               /* File system object for logical drive */
+
+#ifdef __ICCARM__
+#pragma data_alignment=32
+BYTE Buff[16] ;                   /* Working buffer */
+DMA_DESC_T DMA_DESC[2];
+#else
+BYTE Buff[16] __attribute__((aligned(32)));       /* Working buffer */
+DMA_DESC_T DMA_DESC[2] __attribute__((aligned(32)));
+#endif
+
+//uint8_t bAudioPlaying = 0;
+extern signed int aPCMBuffer[2][PCM_BUFFER_SIZE];
+extern volatile uint8_t aPCMBuffer_Full[2];
+volatile uint8_t u8PCMBuffer_Playing=0;
+/*---------------------------------------------------------*/
+/* User Provided RTC Function for FatFs module             */
+/*---------------------------------------------------------*/
+/* This is a real time clock service to be called from     */
+/* FatFs module. Any valid time must be returned even if   */
+/* the system does not support an RTC.                     */
+/* This function is not required in read-only cfg.         */
+
+unsigned long get_fattime (void)
+{
+    unsigned long tmr;
+
+    tmr=0x00000;
+
+    return tmr;
+}
+
+
+void SDH0_IRQHandler(void)
+{
+    unsigned int volatile isr;
+    unsigned int volatile ier;
+
+    // FMI data abort interrupt
+    if (SDH0->GINTSTS & SDH_GINTSTS_DTAIF_Msk)
+    {
+        /* ResetAllEngine() */
+        SDH0->GCTL |= SDH_GCTL_GCTLRST_Msk;
+    }
+
+    //----- SD interrupt status
+    isr = SDH0->INTSTS;
+    if (isr & SDH_INTSTS_BLKDIF_Msk)
+    {
+        // block down
+        SD0.DataReadyFlag = TRUE;
+        SDH0->INTSTS = SDH_INTSTS_BLKDIF_Msk;
+    }
+
+    if (isr & SDH_INTSTS_CDIF_Msk)   // port 0 card detect
+    {
+        //----- SD interrupt status
+        // it is work to delay 50 times for SD_CLK = 200KHz
+        {
+            int volatile i;         // delay 30 fail, 50 OK
+            for (i=0; i<0x500; i++);  // delay to make sure got updated value from REG_SDISR.
+            isr = SDH0->INTSTS;
+        }
+
+        if (isr & SDH_INTSTS_CDSTS_Msk)
+        {
+            printf("\n***** card remove !\n");
+            SD0.IsCardInsert = FALSE;   // SDISR_CD_Card = 1 means card remove for GPIO mode
+            memset(&SD0, 0, sizeof(SDH_INFO_T));
+        }
+        else
+        {
+            printf("***** card insert !\n");
+            SDH_Open(SDH0, CardDetect_From_GPIO);
+            SDH_Probe(SDH0);
+        }
+
+        SDH0->INTSTS = SDH_INTSTS_CDIF_Msk;
+    }
+
+    // CRC error interrupt
+    if (isr & SDH_INTSTS_CRCIF_Msk)
+    {
+        if (!(isr & SDH_INTSTS_CRC16_Msk))
+        {
+            //printf("***** ISR sdioIntHandler(): CRC_16 error !\n");
+            // handle CRC error
+        }
+        else if (!(isr & SDH_INTSTS_CRC7_Msk))
+        {
+            if (!SD0.R3Flag)
+            {
+                //printf("***** ISR sdioIntHandler(): CRC_7 error !\n");
+                // handle CRC error
+            }
+        }
+        SDH0->INTSTS = SDH_INTSTS_CRCIF_Msk;      // clear interrupt flag
+    }
+
+    if (isr & SDH_INTSTS_DITOIF_Msk)
+    {
+        printf("***** ISR: data in timeout !\n");
+        SDH0->INTSTS |= SDH_INTSTS_DITOIF_Msk;
+    }
+
+    // Response in timeout interrupt
+    if (isr & SDH_INTSTS_RTOIF_Msk)
+    {
+        printf("***** ISR: response in timeout !\n");
+        SDH0->INTSTS |= SDH_INTSTS_RTOIF_Msk;
+    }
+}
+
+/*
+	USE_M487_EVM_FREE_I2S
+	PH10 : I2S0_LRCK
+	PH9 : I2S0_DO
+	PH8 : I2S0_DI
+	PE0 : I2S0_MCLK
+	PE1 : I2S0_BCLK
+
+	USE_CUSTOMIZE_BOARD
+	PB1 : I2S0_LRCK
+	PB2 : I2S0_DO
+	PB3 : I2S0_DI
+	PB4 : I2S0_MCLK
+	PB5 : I2S0_BCLK
+*/
+void I2S_Init(void)
+{
+    /* Reset NAU88C10 codec */
+    NAU88C10_Reset();
+
+    /* Open I2S0 interface and set to slave mode, stereo channel, I2S format */
+    I2S_Open(I2S0, I2S_MODE_MASTER, 48000, I2S_DATABIT_16, I2S_ENABLE_MONO, I2S_FORMAT_I2S);//I2S_DISABLE_MONO
+//    NVIC_EnableIRQ(I2S0_IRQn);
+
+    /* Set MCLK and enable MCLK */
+	#if defined (USE_MCLK_12M)
+    I2S_EnableMCLK(I2S0, USE_NAU88C10_12M);
+//    I2S0->CTL0 |= I2S_CTL0_ORDER_Msk;
+
+	#elif defined (USE_MCLK_12_288M)
+    I2S_EnableMCLK(I2S0, USE_NAU88C10_12_288M);
+//    I2S0->CTL0 |= I2S_CTL0_ORDER_Msk;
+	#endif
+
+    /* Initialize NAU88C10 codec */
+    CLK_SysTickDelay(20000);
+    NAU88C10_Setup();
+	
+}
+
+void SD_Init(void)
+{
+    TCHAR       sd_path[] = { '0', ':', 0 };    /* SD drive started from 0 */
+
+    SDH_Open_Disk(SDH0, CardDetect_From_GPIO);
+    f_chdrive(sd_path);          /* set default path */
+}
+
+/*
+
+	USE_M487_EVM_FREE_I2C
+	PC11 : I2C0_SDA
+	PC12 : I2C0_SCL
+
+	USE_CUSTOMIZE_BOARD
+	PA4 : I2C0_SDA
+	PA5 : I2C0_SCL
+
+*/
+void I2C0_Init(void)
+{
+    SYS_ResetModule(I2C0_RST);
+    /* Open I2C0 and set clock to 100k */
+    I2C_Open(I2C0, 100000);
+
+    I2C_SetSlaveAddr(I2C0, 0, DeviceAddr_NAU88C10, I2C_GCMODE_DISABLE);   /* Slave Address : 1101011b */
+
+    /* Get I2C0 Bus Clock */
+    printf("I2C clock %d Hz\n", I2C_GetBusClockFreq(I2C0));
+
+	#if defined (ENABLE_I2C_IRQ)
+    I2C_EnableInt(I2C0);
+    NVIC_EnableIRQ(I2C0_IRQn);	
+	#endif
+}
+
+
+void PDMA_IRQHandler(void)
+{
+    uint32_t u32Status = PDMA_GET_INT_STATUS(PDMA);
+
+    if (u32Status & 0x2)   /* done */
+    {
+        if (PDMA_GET_TD_STS(PDMA) & I2S_PDMA_OPENED_CH)
+        {
+            if(aPCMBuffer_Full[u8PCMBuffer_Playing^1] != 1)
+                printf("underflow!!\n");
+			
+            aPCMBuffer_Full[u8PCMBuffer_Playing] = 0;       //set empty flag
+            u8PCMBuffer_Playing ^= 1;
+			
+//			printf("u8PCMBuffer_Playing : 0x%2X\r\n" , u8PCMBuffer_Playing);			
+        }
+        PDMA_CLR_TD_FLAG(PDMA,PDMA_TDSTS_TDIF2_Msk);
+    }
+    else if(u32Status & 0x400)     /* Timeout */
+    {
+        PDMA_CLR_TMOUT_FLAG(PDMA,PDMA_TDSTS_TDIF2_Msk);
+        printf("PDMA Timeout!!\n");
+    }
+    else
+    {
+        while(1)
+        {
+        	printf("%s : 0x%x\n",__FUNCTION__ , u32Status);
+        }
+    }
+}
+
+// Configure PDMA to Scatter Gather mode */
+void PDMA_Init(void)
+{
+    DMA_DESC[0].ctl = ((PCM_BUFFER_SIZE-1)<<PDMA_DSCT_CTL_TXCNT_Pos)|PDMA_WIDTH_16|PDMA_SAR_INC|PDMA_DAR_FIX|PDMA_REQ_SINGLE|PDMA_OP_SCATTER;
+    DMA_DESC[0].src = (uint32_t)&aPCMBuffer[0][0];
+    DMA_DESC[0].dest = (uint32_t)&I2S0->TXFIFO;
+    DMA_DESC[0].offset = (uint32_t)&DMA_DESC[1] - (PDMA->SCATBA);
+
+    DMA_DESC[1].ctl = ((PCM_BUFFER_SIZE-1)<<PDMA_DSCT_CTL_TXCNT_Pos)|PDMA_WIDTH_16|PDMA_SAR_INC|PDMA_DAR_FIX|PDMA_REQ_SINGLE|PDMA_OP_SCATTER;
+    DMA_DESC[1].src = (uint32_t)&aPCMBuffer[1][0];
+    DMA_DESC[1].dest = (uint32_t)&I2S0->TXFIFO;
+    DMA_DESC[1].offset = (uint32_t)&DMA_DESC[0] - (PDMA->SCATBA);
+
+    PDMA_Open(PDMA, BIT0 << PDMA_CH_I2S);
+    PDMA_SetTransferMode(PDMA, PDMA_CH_I2S, PDMA_I2S0_TX, 1, (uint32_t)&DMA_DESC[0]);
+
+    PDMA_EnableInt(PDMA, PDMA_CH_I2S, PDMA_INT_TRANS_DONE);
+    NVIC_EnableIRQ(PDMA_IRQn);
+
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	printf("\r\n\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHCLKFreq : %8d\r\n",CLK_GetHCLKFreq());	
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());
+
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+
+    /* Set both PCLK0 and PCLK1 as HCLK/2 */
+    CLK->PCLKDIV = CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2;
+
+    /* Enable UART module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Enable PDMA module clock */
+    CLK_EnableModuleClock(PDMA_MODULE);
+
+    /* Select UART module clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
+    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Init I2C */
+	#if defined (USE_M487_EVM_FREE_I2C)
+    CLK_EnableModuleClock(I2C0_MODULE);
+
+    SYS->GPC_MFPH &= ~(SYS_GPC_MFPH_PC11MFP_Msk | SYS_GPC_MFPH_PC12MFP_Msk);
+    SYS->GPC_MFPH |= (SYS_GPC_MFPH_PC11MFP_I2C0_SDA | SYS_GPC_MFPH_PC12MFP_I2C0_SCL);
+
+    PC->SMTEN |= GPIO_SMTEN_SMTEN12_Msk;	//I2C0_SCL
+
+	#elif defined (USE_CUSTOMIZE_BOARD)
+    CLK_EnableModuleClock(I2C0_MODULE);
+
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA4MFP_Msk | SYS_GPA_MFPL_PA5MFP_Msk);
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA4MFP_I2C0_SDA | SYS_GPA_MFPL_PA5MFP_I2C0_SCL);
+
+    PA->SMTEN |= GPIO_SMTEN_SMTEN5_Msk;	//I2C0_SCL
+    #endif
+
+
+    /* Init I2S */
+	#if defined (USE_M487_EVM_FREE_I2S)
+    SYS->GPE_MFPL = (SYS->GPE_MFPL & ~(SYS_GPE_MFPL_PE0MFP_Msk|SYS_GPE_MFPL_PE1MFP_Msk)) |
+                    (SYS_GPE_MFPL_PE0MFP_I2S0_MCLK|SYS_GPE_MFPL_PE1MFP_I2S0_BCLK);
+    SYS->GPH_MFPH = (SYS->GPH_MFPH & ~(SYS_GPH_MFPH_PH8MFP_Msk|SYS_GPH_MFPH_PH9MFP_Msk|SYS_GPH_MFPH_PH10MFP_Msk)) |
+                    (SYS_GPH_MFPH_PH10MFP_I2S0_LRCK|SYS_GPH_MFPH_PH9MFP_I2S0_DO|SYS_GPH_MFPH_PH8MFP_I2S0_DI );
+
+    PE->SMTEN |= GPIO_SMTEN_SMTEN1_Msk;	//I2S0_BCLK						
+
+	#elif defined (USE_CUSTOMIZE_BOARD)
+    SYS->GPB_MFPL &= ~(SYS_GPB_MFPL_PB1MFP_Msk | SYS_GPB_MFPL_PB2MFP_Msk| SYS_GPB_MFPL_PB3MFP_Msk| SYS_GPB_MFPL_PB4MFP_Msk| SYS_GPB_MFPL_PB5MFP_Msk);
+    SYS->GPB_MFPL |= (SYS_GPB_MFPL_PB1MFP_I2S0_LRCK | SYS_GPB_MFPL_PB2MFP_I2S0_DO| SYS_GPB_MFPL_PB3MFP_I2S0_DI| SYS_GPB_MFPL_PB4MFP_I2S0_MCLK| SYS_GPB_MFPL_PB5MFP_I2S0_BCLK);
+
+    PB->SMTEN |= GPIO_SMTEN_SMTEN5_Msk;	//I2S0_BCLK		
+	#endif
+
+    /* Select IP clock source */
+    CLK_SetModuleClock(I2S0_MODULE, CLK_CLKSEL3_I2S0SEL_HXT, 0);
+    /* Enable I2S0 module clock */
+    CLK_EnableModuleClock(I2S0_MODULE);
+	
+    /* Init SD */
+    /* select multi-function pin */
+    SYS->GPE_MFPL &= ~(SYS_GPE_MFPL_PE7MFP_Msk     | SYS_GPE_MFPL_PE6MFP_Msk     | SYS_GPE_MFPL_PE3MFP_Msk      | SYS_GPE_MFPL_PE2MFP_Msk);
+    SYS->GPE_MFPL |=  (SYS_GPE_MFPL_PE7MFP_SD0_CMD | SYS_GPE_MFPL_PE6MFP_SD0_CLK | SYS_GPE_MFPL_PE3MFP_SD0_DAT1 | SYS_GPE_MFPL_PE2MFP_SD0_DAT0);
+
+    SYS->GPB_MFPL &= ~(SYS_GPB_MFPL_PB5MFP_Msk      | SYS_GPB_MFPL_PB4MFP_Msk);
+    SYS->GPB_MFPL |=  (SYS_GPB_MFPL_PB5MFP_SD0_DAT3 | SYS_GPB_MFPL_PB4MFP_SD0_DAT2);
+
+    SYS->GPD_MFPH &= ~(SYS_GPD_MFPH_PD13MFP_Msk);
+    SYS->GPD_MFPH |=  (SYS_GPD_MFPH_PD13MFP_SD0_nCD);
+
+    /* Select IP clock source */
+    CLK_SetModuleClock(SDH0_MODULE, CLK_CLKSEL0_SDH0SEL_PLL, CLK_CLKDIV0_SDH0(10));
+    /* Enable IP clock */
+    CLK_EnableModuleClock(SDH0_MODULE);
+
+    /* Lock protected registers */
+    SYS_LockReg();	
+	
+}
+
+int32_t main (void)
+{
+
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init();
+
+    UART0_Init();	
+    SD_Init();
+    I2C0_Init();
+    I2S_Init();
+	
+    MP3Player();
+
+    while(1);
+}
+
+/*** (C) COPYRIGHT 2014 Nuvoton Technology Corp. ***/
---
+++ SampleCode/Template/nau88C10.c
@@ -0,0 +1,338 @@
+/**************************************************************************//**
+ * @file     NAU88C10.c
+ * @version  V1.00
+ * @brief    M480 I2S Driver Sample Code
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+
+#include "i2c_master.h"
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+#define NAU8810_REG_RESET			0x00
+#define NAU8810_REG_POWER1			0x01
+#define NAU8810_REG_POWER2			0x02
+#define NAU8810_REG_POWER3			0x03
+#define NAU8810_REG_IFACE			0x04
+#define NAU8810_REG_COMP			0x05
+#define NAU8810_REG_CLOCK			0x06
+#define NAU8810_REG_SMPLR			0x07
+#define NAU8810_REG_DAC				0x0A
+#define NAU8810_REG_DACGAIN		0x0B
+#define NAU8810_REG_ADC				0x0E
+#define NAU8810_REG_ADCGAIN		0x0F
+#define NAU8810_REG_EQ1				0x12
+#define NAU8810_REG_EQ2				0x13
+#define NAU8810_REG_EQ3				0x14
+#define NAU8810_REG_EQ4				0x15
+#define NAU8810_REG_EQ5				0x16
+#define NAU8810_REG_DACLIM1		0x18
+#define NAU8810_REG_DACLIM2		0x19
+#define NAU8810_REG_NOTCH1			0x1B
+#define NAU8810_REG_NOTCH2			0x1C
+#define NAU8810_REG_NOTCH3			0x1D
+#define NAU8810_REG_NOTCH4			0x1E
+#define NAU8810_REG_ALC1			0x20
+#define NAU8810_REG_ALC2			0x21
+#define NAU8810_REG_ALC3			0x22
+#define NAU8810_REG_NOISEGATE		0x23
+#define NAU8810_REG_PLLN			0x24
+#define NAU8810_REG_PLLK1			0x25
+#define NAU8810_REG_PLLK2			0x26
+#define NAU8810_REG_PLLK3			0x27
+#define NAU8810_REG_ATTEN			0x28
+#define NAU8810_REG_INPUT_SIGNAL	0x2C
+#define NAU8810_REG_PGAGAIN		0x2D
+#define NAU8810_REG_ADCBOOST		0x2F
+#define NAU8810_REG_OUTPUT			0x31
+#define NAU8810_REG_SPKMIX			0x32
+#define NAU8810_REG_SPKGAIN			0x36
+#define NAU8810_REG_MONOMIX		0x38
+#define NAU8810_REG_POWER4			0x3A
+#define NAU8810_REG_TSLOTCTL1		0x3B
+#define NAU8810_REG_TSLOTCTL2		0x3C
+#define NAU8810_REG_DEVICE_REVID	0x3E
+#define NAU8810_REG_I2C_DEVICEID		0x3F
+#define NAU8810_REG_ADDITIONID		0x40
+#define NAU8810_REG_RESERVE			0x41
+#define NAU8810_REG_OUTCTL			0x45
+#define NAU8810_REG_ALC1ENHAN1		0x46
+#define NAU8810_REG_ALC1ENHAN2		0x47
+#define NAU8810_REG_MISCCTL			0x49
+#define NAU8810_REG_OUTTIEOFF		0x4B
+#define NAU8810_REG_AGCP2POUT		0x4C
+#define NAU8810_REG_AGCPOUT		0x4D
+#define NAU8810_REG_AMTCTL			0x4E
+#define NAU8810_REG_OUTTIEOFFMAN	0x4F
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+void RecoveryFromArbLost(void)
+{
+    I2C0->CTL0 &= ~I2C_CTL0_I2CEN_Msk;
+    I2C0->CTL0 |= I2C_CTL0_I2CEN_Msk;
+}
+
+uint8_t I2C_WriteNAU88C10(uint8_t u8addr, uint16_t u16data)
+{		
+	I2C_T *i2c = MASTER_I2C;	
+
+    uint8_t Device_Address  = 0;
+	uint8_t buf[2] = 0;
+	uint16_t vaule = 0;
+    uint8_t reg_addr  = 0;
+    uint8_t reg_data  = 0;
+	
+	Device_Address = DeviceAddr_NAU88C10 >> 1;
+
+	#if 1	
+	reg_addr = ((u8addr << 1) | (u16data >> 8));
+	reg_data = (u16data & 0x00FF) ;
+
+	buf[0] = reg_addr;
+	buf[1] = reg_data;		
+	#else
+	vaule = (u8addr << 9) | u16data ;
+	buf[0] = vaule >> 8;
+	buf[1] = (uint8_t) (vaule & 0xFF);	
+	#endif
+	
+//	printf("Address : 0x%2X , register : 0x%4X , data : 0x%4X , (0x%4X ,0x%4X , 0x%4X ) \r\n" , Device_Address , u8addr , u16data ,  vaule , buf[0]  , buf[1]);
+
+
+	#if defined (ENABLE_I2C_IRQ)
+//	I2Cx_WriteMultiToSlaveIRQ(Device_Address, buf[0], &buf[1] , 1);
+	I2Cx_WriteMultiToSlaveIRQ(Device_Address, reg_addr , &reg_data , 1);
+	
+	#elif defined (ENABLE_I2C_POLLING_API)
+//	I2C_WriteMultiBytes(i2c, Device_Address , buf, 2);
+	I2C_WriteByteOneReg(i2c, Device_Address , reg_addr, reg_data );	
+
+	#elif defined (ENABLE_I2C_POLLING_DISCRETE)
+
+	#if 1
+restart:
+    I2C_START(i2c);
+    I2C_WAIT_READY(i2c);
+
+    I2C_SET_DATA(i2c, Device_Address << 1);
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+    if(I2C_GET_STATUS(i2c) == 0x38)
+    {
+        RecoveryFromArbLost();
+        goto restart;
+    }
+    else if(I2C_GET_STATUS(i2c) != 0x18)
+        goto stop;
+
+    I2C_SET_DATA(i2c, (uint8_t)reg_addr);
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+    if(I2C_GET_STATUS(i2c) == 0x38)
+    {
+        RecoveryFromArbLost();
+        goto restart;
+    }
+    else if(I2C_GET_STATUS(i2c) != 0x28)
+        goto stop;
+
+    I2C_SET_DATA(i2c, (uint8_t)reg_data);
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+    if(I2C_GET_STATUS(i2c) == 0x38)
+    {
+        RecoveryFromArbLost();
+        goto restart;
+    }
+    else if(I2C_GET_STATUS(i2c) != 0x28)
+        goto stop;
+
+stop:
+    I2C_STOP(i2c);
+    while(i2c->CTL0 & I2C_CTL0_STO_Msk);
+	#else
+    I2C_START(i2c);
+    I2C_WAIT_READY(i2c);
+
+    I2C_SET_DATA(i2c, Device_Address << 1 );
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+
+    I2C_SET_DATA(i2c, (uint8_t)reg_addr);
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+
+    I2C_SET_DATA(i2c, (uint8_t)reg_data);
+    I2C_SET_CONTROL_REG(i2c, I2C_CTL_SI);
+    I2C_WAIT_READY(i2c);
+
+    I2C_STOP(i2c);
+	#endif
+	
+	#endif
+
+
+	return 1;
+
+}
+
+/* config play sampling rate */
+void NAU88C10_ConfigSampleRate(uint32_t u32SampleRate)
+{
+    printf("[NAU88C10] Configure Sampling Rate to %d\n", u32SampleRate);
+
+	#if 0
+
+    if((u32SampleRate % 8) == 0)
+    {
+        I2C_WriteNAU88C10(0x0005, 0x3126); //12.288Mhz
+        I2C_WriteNAU88C10(0x0006, 0x0008);
+    }
+    else
+    {
+        I2C_WriteNAU88C10(0x0005, 0x86C2); //11.2896Mhz
+        I2C_WriteNAU88C10(0x0006, 0x0007);
+    }
+
+    switch (u32SampleRate)
+    {
+    case 16000:
+        I2C_WriteNAU88C10(0x0003,  0x801B); //MCLK = SYSCLK_SRC/12
+        I2C_WriteNAU88C10(0x0004,  0x0001);
+        I2C_WriteNAU88C10(0x0005,  0x3126); //MCLK = 4.096MHz
+        I2C_WriteNAU88C10(0x0006,  0x0008);
+        I2C_WriteNAU88C10(0x001D,  0x301A); //301A:Master, BCLK_DIV=MCLK/8=512K, LRC_DIV=512K/32=16K
+        I2C_WriteNAU88C10(0x002B,  0x0002);
+        I2C_WriteNAU88C10(0x002C,  0x0082);
+        break;
+
+    case 44100:
+        I2C_WriteNAU88C10(0x001D,  0x301A); //301A:Master, BCLK_DIV=11.2896M/8=1.4112M, LRC_DIV=1.4112M/32=44.1K
+        I2C_WriteNAU88C10(0x002B,  0x0012);
+        I2C_WriteNAU88C10(0x002C,  0x0082);
+        break;
+
+    case 48000:
+        I2C_WriteNAU88C10(0x001D,  0x301A); //301A:Master, BCLK_DIV=12.288M/8=1.536M, LRC_DIV=1.536M/32=48K
+        I2C_WriteNAU88C10(0x002B,  0x0012);
+        I2C_WriteNAU88C10(0x002C,  0x0082);
+        break;
+
+    case 96000:
+        I2C_WriteNAU88C10(0x0003,  0x80A2); //MCLK = SYSCLK_SRC/2
+        I2C_WriteNAU88C10(0x0004,  0x1801);
+        I2C_WriteNAU88C10(0x0005,  0x3126); //MCLK = 24.576MHz
+        I2C_WriteNAU88C10(0x0006,  0xF008);
+        I2C_WriteNAU88C10(0x001D,  0x301A); //3019:Master, BCLK_DIV=MCLK/8=3.072M, LRC_DIV=3.072M/32=96K
+        I2C_WriteNAU88C10(0x002B,  0x0001);
+        I2C_WriteNAU88C10(0x002C,  0x0080);
+        break;
+    }
+
+	#endif
+}
+
+/*
+13.1. SOFTWARE RESET 
+This is device Reset register.  Performing a write instruction to this register with any data will reset all the bits in the 
+register map to default. 
+
+*/
+
+void NAU88C10_Reset(void)
+{
+    I2C_WriteNAU88C10(0,  0x1);
+    I2C_WriteNAU88C10(0,  0);   // Reset all registers
+    CLK_SysTickDelay(10000);
+
+    printf("NAU88C10 Software Reset.\n");
+}
+
+
+void NAU88C10_Setup(void)
+{
+	uint8_t rx[2]={0};
+
+	I2C_WriteNAU88C10(0x00 , 0x000);
+    CLK_SysTickDelay(10000);
+	
+	I2C_WriteNAU88C10(0x01 , 0x11F);
+	I2C_WriteNAU88C10(0x02 , 0x015);
+	I2C_WriteNAU88C10(0x03 , 0x065);
+	I2C_WriteNAU88C10(0x04 , BIT4);		//I2C_WriteNAU88C10(0x04 , BIT4|BIT3);	
+	I2C_WriteNAU88C10(0x05 , 0x000);
+    CLK_SysTickDelay(10000);
+	
+	I2C_WriteNAU88C10(0x06 , 0x00C);
+	I2C_WriteNAU88C10(0x07 , 0x000);	//BIT2|BIT1
+	I2C_WriteNAU88C10(0x0A , 0x008);
+	I2C_WriteNAU88C10(0x0B , 0x0FF);
+	I2C_WriteNAU88C10(0x0E , 0x108);
+	I2C_WriteNAU88C10(0x0F , 0x0FF);
+	I2C_WriteNAU88C10(0x12 , 0x12C);
+	I2C_WriteNAU88C10(0x13 , 0x02C);
+	I2C_WriteNAU88C10(0x14 , 0x02C);
+	I2C_WriteNAU88C10(0x15 , 0x02C);
+	I2C_WriteNAU88C10(0x16 , 0x02C);
+	I2C_WriteNAU88C10(0x18 , 0x032);
+	I2C_WriteNAU88C10(0x19 , 0x000);
+	I2C_WriteNAU88C10(0x1B , 0x000);
+	I2C_WriteNAU88C10(0x1C , 0x000);
+	I2C_WriteNAU88C10(0x1D , 0x000);
+	I2C_WriteNAU88C10(0x1E , 0x000);
+	I2C_WriteNAU88C10(0x20 , 0x038);
+	I2C_WriteNAU88C10(0x21 , 0x00B);
+	I2C_WriteNAU88C10(0x22 , 0x032);
+	I2C_WriteNAU88C10(0x23 , 0x000);
+
+	#if defined (USE_MCLK_12_288M)	// 12.844
+	I2C_WriteNAU88C10(0x24 , 0x008);
+	I2C_WriteNAU88C10(0x25 , 0x00C);
+	I2C_WriteNAU88C10(0x26 , 0x093);
+	I2C_WriteNAU88C10(0x27 , 0x0E9);
+	
+	#elif defined (USE_MCLK_12M)	// 12M
+	
+	I2C_WriteNAU88C10(0x24 , 0x006);
+	I2C_WriteNAU88C10(0x25 , 0x009);
+	I2C_WriteNAU88C10(0x26 , 0x06E);
+	I2C_WriteNAU88C10(0x27 , 0x12F);
+	#endif
+	
+	I2C_WriteNAU88C10(0x28 , 0x000);
+	I2C_WriteNAU88C10(0x2C , 0x003);
+	I2C_WriteNAU88C10(0x2D , 0x010);
+	I2C_WriteNAU88C10(0x2F , 0x100);
+	I2C_WriteNAU88C10(0x31 , 0x00E);
+	I2C_WriteNAU88C10(0x32 , 0x001);
+	I2C_WriteNAU88C10(0x36 , 0x039);
+	I2C_WriteNAU88C10(0x38 , 0x000);
+	I2C_WriteNAU88C10(0x3A , 0x000);
+	I2C_WriteNAU88C10(0x3B , 0x000);
+	I2C_WriteNAU88C10(0x3C , 0x020);
+	I2C_WriteNAU88C10(0x3E , 0x0EE);
+	I2C_WriteNAU88C10(0x3F , 0x01A);
+	I2C_WriteNAU88C10(0x40 , 0x0CA);
+	I2C_WriteNAU88C10(0x41 , 0x1FF);
+	I2C_WriteNAU88C10(0x45 , 0x1FF);
+	I2C_WriteNAU88C10(0x46 , 0x064);
+	I2C_WriteNAU88C10(0x47 , 0x1FF);
+	I2C_WriteNAU88C10(0x49 , 0x000);
+	I2C_WriteNAU88C10(0x4B , 0x1FF);
+	I2C_WriteNAU88C10(0x4C , 0x1FF);
+	I2C_WriteNAU88C10(0x4D , 0x001);
+	I2C_WriteNAU88C10(0x4E , 0x1FF);
+	I2C_WriteNAU88C10(0x4F , 0x1FF);
+
+    printf("NAU88C10 Configured done.\n");
+
+}
+
+
